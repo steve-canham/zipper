8d386cebf7c66e148e44508c230be22eeb5f3948
Steve Canham <stevecanham@outlook.com>
2025-02-25 22:45:29 +0000

    Adapted to revised DB schemas

--- src/data.rs
+++ src/data.rs
@@ -5,7 +5,7 @@ use sqlx::{Pool, Postgres};
 
 pub async fn get_all_ids(pool: &Pool<Postgres>) -> Result<Vec<i32>, AppError> {
 
-    let sql = r#"select id from sf.source_parameters
+    let sql = r#"select id from source.parameters
     where id > 100115 and id not in (100159, 101405, 101940, 110426)
     order by preference_rating"#;
     sqlx::query_scalar(sql).fetch_all(pool).await
@@ -16,7 +16,7 @@ pub async fn get_all_ids(pool: &Pool<Postgres>) -> Result<Vec<i32>, AppError> {
 pub async fn get_source_details(source_id: i32, pool: &Pool<Postgres>) -> Result<SourceDetails, AppError> {
 
     let sql = r#"select id, database_name, local_folder, 
-    local_files_grouped, grouping_range_by_id from sf.source_parameters
+    local_files_grouped, grouping_range_by_id from source.parameters
     where id = $1"#;
     sqlx::query_as(sql).bind(source_id).fetch_one(pool).await
              .map_err(|e| AppError::SqlxError(e, sql.to_string()))
